da7e2baae894e13e7a954a912eec0bba7fc017c4
A rashid <coxringcox@gmail.com>
2026-06-23 14:05:01 +0600

    Thire is modified here

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/shared/Navbar";
-import Footer from "@/components/shared/Footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,14 +20,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      data-theme="light"
-      className={`${geistSans.variable} ${geistMono.variable} h-full light antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <Navbar />
-        {children}
-        <Footer />
-        </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
